b9091dcb82114e6d50b6a0af77dccc591eb0d665
Andrey <k18041990@gmail.com>
2022-06-09 08:45:24 +0300

    My first commit

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,11 +9,94 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var scrollView: UIScrollView!
+    
+    @IBOutlet var login: UITextField!
+    @IBOutlet var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        scrollView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideKeyboard)))
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Подписываемся на два уведомления: одно приходит при появлении клавиатуры
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+        // Второе — когда она пропадает
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        let apply = checkDataOfUser(withIdentifier: identifier)
+        
+        if !apply {
+            showLoginError()
+        }
+        
+        return apply
+    }
+    
+    private func checkDataOfUser(withIdentifier identifier: String) -> Bool {
+        var apply = true
+        
+        if identifier == "toListCities" {
+            if let login = self.login.text, login == ""
+                ,let password = self.password.text, password == "" {
+                
+                apply = true
+            } else {
+                apply = false
+            }
+        }
+        
+        return apply
+    }
+    
+    @objc func hideKeyboard() {
+        scrollView.endEditing(true)
+    }
+    
+    private func showLoginError() {
+        let alert = UIAlertController(title: "Error login", message: "Incorect user data", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        
+        self.present(alert, animated: true)
     }
 
 
+    // Когда клавиатура появляется
+    @objc func keyboardWasShown(notification: Notification) {
+        // Получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        self.scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    //Когда клавиатура исчезает
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        // Устанавливаем отступ внизу UIScrollView, равный 0
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+    }
+    
+    
+    @IBAction func pressLogin(_ sender: UIButton) {
+        //myView.endEditing(true)
+    }
+    
 }
 
